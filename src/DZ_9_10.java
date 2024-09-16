8fcd8aeaba144130bb9f57673f3ce93380e66a40
DZ <neitie221@gmail.com>
2024-09-16 21:12:38 +0800

    Домашняя работа

---
+++ src/DZ_9_10.java
@@ -0,0 +1,33 @@
+public class DZ_9_10 {
+    public static void main(String[] args) {
+        System.out.println("Начало");
+        System.out.println("Заварить кофе");
+        System.out.println("Залить воду в чайник");
+        System.out.println("Нажать кнопку Вскипятить");
+        System.out.println("Подождать");
+
+        int num1 = 100;
+        int num2 = 200;
+
+        if (num1 < num2) {
+            System.out.println("Вода вскипятилась");
+        }   else {
+                System.out.println("Вода не вскипятилась");
+            }
+        System.out.println("Выключить чайник");
+        System.out.println("Засыпать кофе в чашку");
+        System.out.println("Залить воду в чайник");
+        System.out.println("Помешать");
+
+        int num3 = 150;
+        int num4 = 250;
+
+        if (num3 < num4) {
+            System.out.println("Кофе остыло");
+        }   else {
+                 System.out.println("Кофе не остыло");
+        }
+        System.out.println("Выпить кофе");
+        System.out.println("Конец");
+    }
+}
