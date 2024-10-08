eb2c7d65eb41e5ffe3c6cbece828169bb595ec4f
DZ <neitie221@gmail.com>
2024-10-09 01:23:53 +0800

    дз

---
+++ src/Ucheba/DZ_20_21/Main.java
@@ -0,0 +1,30 @@
+package Ucheba.DZ_20_21;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 14;
+        int num2 = 25;
+        int num3 = 36;
+        System.out.println(Method.square(num1));
+        System.out.println(Method.square(num2));
+        System.out.println(Method.square(num3));
+
+        Method.printFullName();
+
+        String[] words = new String[7];
+        words[0] = "Bucket";
+        words[1] = "Banana";
+        words[2] = "Egg";
+        words[3] = "Objectivization";
+        words[4] = "Tree";
+        words[5] = "Globus";
+        words[6] = "Eagle";
+        System.out.println(Arrays.toString(Method.cornerValuesArray(words)));
+
+        String[] words1 = new String[4];
+        words1[0] = "Bucket";
+        words1[1] = "Banana";
+    }
+}
---
+++ src/Ucheba/DZ_20_21/Method.java
@@ -0,0 +1,33 @@
+package Ucheba.DZ_20_21;
+
+import java.util.Arrays;
+
+public class Method {
+    public static int square;
+    private static int[][] String;
+
+    public static int square(int value) {
+        return value * value;
+    }
+
+    public static void printFullName() {
+        String firstName = "Ivan";
+        String secondName = "Petrov";
+        System.out.println(firstName + " " + secondName);
+    }
+
+    static String[] cornerValuesArray(String[] words) {
+        String min = words[0];
+        String max = words[0];
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() < min.length()) {
+                min = words[i];
+            } else if (words[i].length() > max.length()) {
+                max = words[i];
+            }
+        }
+        return new String[]{min, max};
+    }
+}
+
