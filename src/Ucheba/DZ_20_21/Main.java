bf705f99ae4928f4f7dbad977e40772fba58018e
DZ <neitie221@gmail.com>
2024-10-09 01:43:27 +0800

    commit1

--- src/Ucheba/DZ_20_21/Main.java
+++ src/Ucheba/DZ_20_21/Main.java
@@ -22,9 +22,5 @@ public class Main {
         words[5] = "Globus";
         words[6] = "Eagle";
         System.out.println(Arrays.toString(Method.cornerValuesArray(words)));
-
-        String[] words1 = new String[4];
-        words1[0] = "Bucket";
-        words1[1] = "Banana";
     }
 }
